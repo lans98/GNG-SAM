64998d18d735df9df77d6a4f37ab81f8445f192b
lans98 <lans9831@gmail.com>
2018-05-23 19:25:17 -0500

    random signal

--- src/include/gng_sam.hpp
+++ src/include/gng_sam.hpp
@@ -7,6 +7,7 @@
 
 // Crate dependencies
 #include <point.hpp>
+#include <random.hpp>
 
 namespace gng {
 
@@ -32,7 +33,8 @@ namespace gng {
     GNG() = default;
     GNG(const GNG&) = delete;
 
-    void start() {
+    // stop criterion, net size
+    void start(size_t desired_netsize) {
       Node* tmp_node;
 
       tmp_node = new Node();
@@ -43,6 +45,20 @@ namespace gng {
       tmp_node->point = PointN<N>::random_in(MIN_DOUBLE, MAX_DOUBLE);
       nodes.push_back(tmp_node);
 
+      nodes[0]->relatives.push_back(nodes[1]);
+      nodes[1]->relatives.push_back(nodes[0]);
+
+      while (nodes < desired_netsize) {
+        double signal = gen_random_signal();
+
+      }
+    }
+
+
+  private:
+
+    double gen_random_signal() {
+      return random::random_in(MIN_DOUBLE, MAX_DOUBLE);
     }
 
   };
