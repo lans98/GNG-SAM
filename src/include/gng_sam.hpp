aec77fa387cae6539cab8fc1f5ee693990783988
lans98 <lans9831@gmail.com>
2018-05-24 10:23:23 -0500

    weaita

--- src/include/gng_sam.hpp
+++ src/include/gng_sam.hpp
@@ -2,6 +2,7 @@
 #define GNG_SAM_HPP
 
 // Std dependencies
+#include <utility>
 #include <vector>
 #include <numeric>
 
@@ -14,6 +15,8 @@ namespace gng {
   using namespace std;
   using namespace gng::point;
 
+  using Age = unsigned long;
+
   constexpr double MIN_DOUBLE = numeric_limits<double>::min();
   constexpr double MAX_DOUBLE = numeric_limits<double>::max();
 
@@ -21,14 +24,21 @@ namespace gng {
   class GNG {
   private:
     // Related abstractions
+    struct Node;
+
+    struct Edge {
+      Age   age;
+      Node* rela;
+    };
+
     struct Node {
       double        error;
       PointN<N>     point;
-      vector<Node*> relatives;
+      vector<Edge>  relatives;
     };
 
     // Private fields
-    vector<Node*> nodes;
+    vector<Node*> nodes; // the net
 
   public:
     GNG() = default;
@@ -46,20 +56,26 @@ namespace gng {
       tmp_node->point = PointN<N>::random_in(MIN_DOUBLE, MAX_DOUBLE);
       nodes.push_back(tmp_node);
 
-      nodes[0]->relatives.push_back(nodes[1]);
-      nodes[1]->relatives.push_back(nodes[0]);
+      nodes[0]->relatives.push_back(Edge { .age = 0, .rela = nodes[1] });
+      nodes[1]->relatives.push_back(Edge { .age = 0, .rela = nodes[0] });
 
       while (nodes.size() < desired_netsize) {
         PointN<N> signal = gen_random_signal();
+        auto [v, u] = two_nearest_nodes(signal);
 
+        for (auto& n : v.relatives)
+          n.age += 1;
       }
     }
-//nose que weas
 
   private:
 
     PointN<N> gen_random_signal() {
       return PointN<N>::random_in(MIN_DOUBLE, MAX_DOUBLE);
+    }
+
+    pair<Node*, Node*> two_nearest_nodes(const PointN<N>& point) {
+
     }
 
   };
